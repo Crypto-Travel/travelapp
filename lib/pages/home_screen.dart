3eb0a3ec96d007a0a12eb98f15d2f972ce1f32a0
Piff51 <117763251+Piff51@users.noreply.github.com>
2023-03-05 17:39:33 +0100

    asd
    risoluzione problemi personali

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -1,6 +1,5 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
+import 'package:travelapp/utilities/colors.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -12,6 +11,58 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: kWhiteClr,
+      body: SizedBox(
+        width: double.infinity,
+        height: double.infinity,
+        child: SafeArea(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              const Positioned(
+                top: 20,
+                left: 20,
+                child: Text(
+                  "Hi, Username",
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontStyle: FontStyle.normal,
+                  ),
+                ),
+              ),
+              Positioned(
+                top: 20,
+                right: 20,
+                child: Image.asset(
+                  "assets/icons/Google.png",
+                  height: 35,
+                  width: 35,
+                ),
+              ),
+              Positioned(
+                child: Container(
+                  alignment: Alignment.topCenter,
+                  padding: const EdgeInsets.fromLTRB(25, 70, 25, 0),
+                  child: TextField(
+                    decoration: InputDecoration(
+                      hintText: "Naviga con noi!!!!!!!!!",
+                      border: const OutlineInputBorder(),
+                      prefixIcon: const Icon(
+                        IconData(0xe567, fontFamily: 'MaterialIcons'),
+                      ),
+                      suffixIcon: IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Icons.clear),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
