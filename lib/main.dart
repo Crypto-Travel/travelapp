3ac47a1e5be3c6baa594f1f30f7689d1deac5beb
Matteo Grifantini <matteo.grifantini@gmail.com>
2023-03-05 20:14:23 +0100

    init questionarioPage

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:travelapp/pages/welcomeScreen.dart';
+import 'package:travelapp/pages/questionarioPage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const IntroPage(),
+      home: const QuestionarioPage(),
     );
   }
 }
